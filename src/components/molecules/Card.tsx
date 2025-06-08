97167b17dc93ae1264b7072a16fc2d7c6d053e01
Bismark <agyarebismark6@gmail.com>
2025-06-08 15:23:49 +0000

    Refactor ProductCard component to enhance structure and improve image handling

--- src/components/molecules/Card.tsx
+++ src/components/molecules/Card.tsx
@@ -1,36 +1,56 @@
-import { Box, Image, Text } from "@mantine/core";
-import React from "react";
+import { Card, Image, Title, Center } from "@mantine/core";
+import AppButton from "../atoms/AppButton";
 
-function Card() {
+function ProductCard({ title, image }: { title: string; image: string }) {
   return (
-    <>
-      <div style={{ maxWidth: 350, height: 284, margin: "0 auto" }}>
-        {/* <Image src="/images/1.jpg" alt="Card" width={300} height={300} /> */}
-        <div
-          style={{
-            margin: "0 auto",
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
-          <Image
-            radius="md"
-            h={80}
-            w="auto"
-            fit="contain"
-            src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-9.png"
-            alt="Card"
-          />
-        </div>
-        <Box bg="gray.3" style={{ width: "100%" }} pt={16} pb={16}>
-          <div style={{ fontWeight: 600, fontSize: 18, marginBottom: 8 }}>
-            Card title
-          </div>
-          <Text>Card description</Text>
-        </Box>
+    <Card
+      // shadow="sm"
+      padding="lg"
+      radius="md"
+      bg={"#f2f2f2"}
+      style={{ backgroundColor: "#f2f2f2" }}
+    >
+      <Card.Section
+        bg={"tertiary.3"}
+        w={350}
+        h={318}
+        style={{
+          display: "flex",
+          justifyItems: "center",
+          alignItems: "center",
+          borderRadius: 12,
+        }}
+      >
+        <Image
+          src={
+            image ||
+            "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+          }
+          height={172}
+          width={143}
+          style={{ objectFit: "contain" }}
+          alt="Norway"
+        />
+      </Card.Section>
+
+      {/* <Group justify="space-between" mt="md" mb="xs">
+        <Text fw={500}>Norway Fjord Adventures</Text>
+        <Badge color="pink">On Sale</Badge>
+      </Group> */}
+      <Title order={3} fw={400} mt={30} c={"primary.3"}>
+        <Center>{title || "xx3"}</Center>
+      </Title>
+
+      {/* <Text size="sm" c="dimmed">
+        With Fjord Tours you can explore more of the magical fjord landscapes
+        with tours and activities on and around the fjords of Norway
+      </Text> */}
+
+      <div style={{ maxWidth: 160, margin: "0 auto" }}>
+        <AppButton mt="md">SEE PRODUCT</AppButton>
       </div>
-    </>
+    </Card>
   );
 }
 
-export default Card;
+export default ProductCard;
