9a35c82a75a0a53bed4203e3e2b2232c5c7cb018
Bismark <agyarebismark6@gmail.com>
2025-06-11 15:31:18 +0000

    refact: removed unused code

--- src/components/molecules/HomeHero.tsx
+++ src/components/molecules/HomeHero.tsx
@@ -28,36 +28,9 @@ export default function HomeHero({ handleClick }: HomeHeroProps) {
               <br /> HEADPHONES
             </Title>
             <Text c="dimmed" mt="md" style={{ maxWidth: 349 }}>
-              {/* Build fully functional accessible web applications faster than
-              ever – Mantine includes more than 120 customizable components and
-              hooks to cover you in any situation */}
               Experience natural, lifelike audio and exceptional build quality
               made for the passionate music enthusiast.
             </Text>
-
-            {/* <List
-              mt={30}
-              spacing="sm"
-              size="sm"
-              icon={
-                <ThemeIcon size={20} radius="xl">
-                  <IconCheck size={12} stroke={1.5} />
-                </ThemeIcon>
-              }
-            >
-              <List.Item>
-                <b>TypeScript based</b> – build type safe applications, all
-                components and hooks export types
-              </List.Item>
-              <List.Item>
-                <b>Free and open source</b> – all packages have MIT license, you
-                can use Mantine in any project
-              </List.Item>
-              <List.Item>
-                <b>No annoying focus ring</b> – focus ring will appear only when
-                user navigates with keyboard
-              </List.Item>
-            </List> */}
 
             <Group mt={30}>
               <AppButton
@@ -67,21 +40,8 @@ export default function HomeHero({ handleClick }: HomeHeroProps) {
               >
                 See product
               </AppButton>
-              {/* <Button radius="xl" size="md" className={classes.control}>
-                Get started
-              </Button> */}
-              {/* <Button
-                variant="default"
-                radius="xl"
-                size="md"
-                className={classes.control}
-              >
-                Source code
-              </Button> */}
             </Group>
           </div>
-
-          {/* <Image src={HeroImage} className={classes.image} /> */}
         </div>
       </Container>
     </Container>
